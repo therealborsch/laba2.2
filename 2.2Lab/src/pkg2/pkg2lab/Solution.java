fa01987c2066f57c03ca1df7e676ad0b97cbb84f
therealborsch <38890058+therealborsch@users.noreply.github.com>
2018-05-01 23:07:36 +0500

    Add files via upload

---
+++ 2.2Lab/src/pkg2/pkg2lab/Solution.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2.pkg2lab;
+
+/**
+ *
+ * @author lende
+ */
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+        for (int i=1; i<=100; i++)
+        {
+
+            if (i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
